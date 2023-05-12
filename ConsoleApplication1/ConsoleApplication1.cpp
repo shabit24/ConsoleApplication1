1d334c6b50498a70da4db98d5a3a0533f5ec8a8d
shabit24 <hossain.shabit24@gmail.com>
2023-05-12 09:49:28 +0700

    binary search function for searching condition

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,30 @@ string input_element() {
     cin >> element;
     return element;
 }
+void binary_search(string arr[], string first_name, string last_name, string element, int size) {
+    int lowerbound = 0;
+    int upperbound = size - 1;
+
+    while (lowerbound <= upperbound) {
+        int mid = (lowerbound + upperbound) / 2;
+
+        if (arr[mid] == element) {
+            cout << "Found" << endl;
+            return;
+        }
+
+        if (element < arr[mid]) {
+            upperbound = mid - 1;
+        }
+        else {
+            lowerbound = mid + 1;
+        }
+    }
+
+    cout << "Not Found" << endl;
+}
+
+
 
 
 
